780924c303a6b409bec8b468997bbf7deaa09d55
Yana <shanna.666@mail.ru>
2020-04-29 21:40:49 +1000

    add opengl1

---
+++ Opengl1/Opengl/ProgClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Opengl
+{
+    public class ProgClass
+    {
+
+        static void Main(string[] args)
+        {
+            var application = new AppClass(800, 600, "«<»>");
+            application.Run(60);
+        }
+    }
+}
